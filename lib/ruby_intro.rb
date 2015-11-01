bcd848c2a13a6f9b2c03145131733f37fa01b8b3
Derf Shaya <derfshaya@gmail.com>
2015-11-01 19:02:56 +0000

    Modified code to better fit the spirit of Ruby

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -8,38 +8,12 @@ def sum arr
 end
 
 def max_2_sum arr
-  return 0 if arr.empty?
-  max = arr.max
-  return max if arr.length == 1
-  max_encountered = false
-  max2 = arr.reject { |i|
-    if !max_encountered && i == max
-      max_encountered = true
-      true  
-    else
-      false
-    end
-  }.max
-  return max + max2
+  sum(arr.sort.pop(2))
 end
 
 def sum_to_n? arr, n
-  # if empty array, a sum of 0 can be achieved
-  return false if arr.empty?
-
-  # nested iterator
-  arr1 = arr.sort
-  found = false
-  0.upto(arr1.length-1) { |idx|
-    x = arr1[idx]
-    0.upto(arr1.length-1) { |idx2|
-      if idx2 != idx
-        y = arr1[idx2]
-        found = true if x+y == n
-      end
-    } if x <= n
-  }
-  return found
+  arr.sort.combination(2).each { |c| return true if sum(c) == n }
+  false
 end
 
 # Part 2
@@ -53,7 +27,7 @@ def starts_with_consonant? s
 end
 
 def binary_multiple_of_4? s
-  return Integer(s, 2) % 4 == 0
+  return Integer(s,2) % 4 == 0
   rescue ArgumentError
     return false
 end
@@ -64,12 +38,8 @@ class BookInStock
   attr_accessor :isbn
   attr_accessor :price
   def initialize(isbn, price)
-    if isbn.empty?
-      raise ArgumentError
-    end
-    if price <= 0
-      raise ArgumentError
-    end
+    raise ArgumentError if isbn.empty?
+    raise ArgumentError if price <= 0
     @isbn = isbn
     @price = price
   end
